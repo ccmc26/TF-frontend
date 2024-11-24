ffe015d9ab2a62ebad91766cf42acefcc29a53a7
carconex <karmeconemar@gmail.com>
2024-11-24 11:40:46 +0100

    canvis en el item i menu, mirant a vore com puc fer per a que apareguen els noms dels tipus i s'imprimisquen tots els items que pertanyen a eixe tipus

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,33 +1,15 @@
 import React from 'react'
-import { useEffect, useState } from 'react'
 import './Item.css'
 import { IoMdAddCircleOutline } from "react-icons/io";
 
-const Item = () => {
-  const apiUrl = process.env.REACT_APP_API_URL;
-  // const apiUrl = "https://tf-backend-production.up.railway.app";
-  const [productos, setProductos] = useState([]);
-  useEffect(() => {
-      // Hacemos la solicitud para obtener los datos desde el backend
-      fetch(`${apiUrl}/api/productos`)
-        .then((response) => response.json())
-        .then((data) => setProductos(data)) // Guardamos los datos en el estado
-        .catch((error) => console.error('Error al cargar productos:', error));
-  },[apiUrl]);
+const Item = ({ producto }) => {
   return (
-    <div className='item'>
-        <h2 className='titol'> LLISTA PLATS </h2>
-        <div className="product-list">
-          {productos.map((producto) => (
-            <div className="product" key={producto._id}>
-              <h2>{producto.name}</h2>
-              <p>{producto.desc} - {producto.price}€</p>
-              <button className='icon-button'>
-                <IoMdAddCircleOutline className='icon-photo'/>
-              </button>
-            </div>
-          ))}
-        </div>
+    <div className="product">
+      <h2>{producto.name}</h2>
+      <p>{producto.desc} - {producto.price}€</p>
+      <button className='icon-button'>
+        <IoMdAddCircleOutline className='icon-photo'/>
+      </button>
     </div>
   )
 }
--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import './CSS/Menu.css';
 import Item from '../Components/Item/Item'
 
 const Menu = () => {
+  const apiUrl = process.env.REACT_APP_API_URL;
+  const [productos, setProductos] = useState([]);
+  const [tipos, setTipos] = useState([]);
+  
+  // fetch per a obtindre tots els tipos ASC
+  useEffect(() => {
+    // Hacemos la solicitud para obtener todos los tipos
+    fetch(`${apiUrl}/api/tipos/asc`)
+      .then((response) => response.json())
+      .then((data) => setTipos(data)) // guarda dades
+      .catch((error) => console.error('Error al cargar tipos:', error));
+  }, [apiUrl]);
+  
+  // fem un fetch per a tindre tots els productes de manera ordenada ASC
+  // utilitzem el useEffect per a controlar la asincronia i asegurar que
+  // soles es fa quan una dependencia canvia
+  useEffect(() => {
+      // Hacemos la solicitud para obtener los datos desde el backend
+      fetch(`${apiUrl}/api/productos/ordered`)
+        .then((response) => response.json())
+        .then((data) => setProductos(data)) // Guardamos los datos en el estado
+        .catch((error) => console.error('Error al cargar productos:', error));
+  },[tipos, apiUrl]);
+  
+  // fem un fetch per a tindre tots els tipus de productes
+
+
   return (
-    <div>
-      <Item />
-    </div>
+    <div> 
+      <div className='item'> 
+        <h2 className='titol'> LLISTA PLATS </h2> 
+        <div className="product-list"> 
+          {productos.map((producto) => ( 
+            <Item key={producto._id} producto={producto} /> 
+          ))} 
+          </div> 
+        </div> 
+      </div>
   )
 }
 
